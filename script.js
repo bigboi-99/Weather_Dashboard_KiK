311c7b88fd1d76bdfdb8b8da76c55f1c19ebcb89
Aditya <adityasinghiitkgp2@gmail.com>
2025-12-18 23:57:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const backToTop = document.getElementById("back_to_top_button");
-
-backToTop.addEventListener("click",function(){
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
